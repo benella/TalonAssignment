fc390b84340c07ec3446d17dc206ad7631512487
Ben Ella <BenElla91@gmail.com>
2022-12-04 14:14:28 +0200

    Documentation

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const Event = require('../models/event')
 const paginate = require('../utils/paginate')
 
+/**
+ * Listing Events as paginated response
+ */
 router.get('/', function (req, res) {
   const query = {}
 
@@ -13,6 +16,9 @@ router.get('/', function (req, res) {
   paginate(Event, query, '-time', req, res)
 })
 
+/**
+ * Listing all distinct values of Event types
+ */
 router.get('/filter-options/eventType', function (req, res) {
   Event.distinct('eventType').then((options) => {
     res.json({ options })
--- server/utils/paginate.js
+++ server/utils/paginate.js
@@ -1,6 +1,17 @@
+const DEFAULT_PAGE_SIZE = 10
+const DEFAULT_OFFSET = 10
+
+/**
+ * Responding with a paginated result of the given query
+ * @param model
+ * @param query
+ * @param defaultSort
+ * @param request
+ * @param response
+ */
 const paginate = (model, query, defaultSort, request, response) => {
-  const pageSize = +request.query.pageSize || 10
-  const offset = +request.query.offset || 0
+  const pageSize = +request.query.pageSize || DEFAULT_PAGE_SIZE
+  const offset = +request.query.offset || DEFAULT_OFFSET
 
   model
     .find(query)
